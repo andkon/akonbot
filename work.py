9abcc07577407cbc358dcc378a8b077bacb75085
Andrew Konoff <andrew.konoff@gmail.com>
2016-09-21 18:07:03 -0700

    Uh I think I made it work

--- work.py
+++ work.py
@@ -5,32 +5,6 @@ import csv
 import glob
 import time
 
-def tidy_up_messages(message_list, i, final_messages):
-	""" 
-	appends messages from the same person together. 
-	"""
-
-	# first, we see if we've reached the end of the message list
-	try:
-		msg = message_list[i]
-	except IndexError:
-		# all done here. loop through and make a normal ass list
-		return map(lambda x: x['Message'], final_messages)
-
-	# now, we either append to the last message, or we add a whole new message.
-	if len(final_messages) > 0:
-		if msg['Name'] == final_messages[-1]['Name']:
-			final_messages[-1]['Message']+= ". %s" % (msg["Message"])
-		else:
-			final_messages.append(msg)
-	elif (len(message_list) - 1) == i:
-		return map(lambda x: x['Message'], final_messages)
-	else:
-		final_messages.append(msg)
-
-	import pdb; pdb.set_trace()
-	i+=1
-	tidy_up_messages(message_list, i, final_messages)
 
 def make_convo_list(filename):
 	""" takes a csv. 
@@ -44,8 +18,10 @@ def make_convo_list(filename):
 	for row in csvFile:
 		if csvFile.line_num == 1:
 			continue
+		elif csvFile.line_num == 2:
+			if row[0] == "Me":
+				break
 		elif csvFile.line_num > 10:
-			# here, if there's an app 
 			if row[0] =="Me":
 				break
 
@@ -61,11 +37,20 @@ def make_convo_list(filename):
 		# now append
 		all_messages.append(di)
 
-	# now check if this stuff is valid:
+	# now make a pretty list and return it
 
-	return tidy_up_messages(all_messages, 0, [])
+	final_messages = []
+	for msg in all_messages:
+		# now, we either append to the last message, or we add a whole new message.
+		if len(final_messages) > 0:
+			if msg['Name'] == final_messages[-1]['Name']:
+				final_messages[-1]['Message']+= ". %s" % (msg["Message"])
+			else:
+				final_messages.append(msg)
+		else:
+			final_messages.append(msg)
 
-
+	return map(lambda x: x['Message'], final_messages)
 
 def make_a_bot():
 	chatbot = ChatBot("Bud")
@@ -77,7 +62,6 @@ def make_a_bot():
 		print('%s' % filename)
 		# create a list 
 		new_list = make_convo_list(filename)
-		import pdb; pdb.set_trace()
 		print new_list
 		if new_list is not None:
 			# train it!
